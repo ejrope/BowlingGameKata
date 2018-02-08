6a9d7a9e0b0a183f517691459e17cfb139c6b1a4
ejrope <ejrope@gmail.com>
2018-02-08 19:17:43 +0100

    Refactor: calculate score

--- src/main/java/com/ejrope/katas/BowlingGame/Game.java
+++ src/main/java/com/ejrope/katas/BowlingGame/Game.java
@@ -1,13 +1,22 @@
 package com.ejrope.katas.BowlingGame;
 
 public class Game {
-    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll] = pins;
+        currentRoll++;
     }
 
     public int score() {
+        int score = 0;
+        int rollIndex = 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+            score += rolls[rollIndex] + rolls[rollIndex + 1];
+            rollIndex += 2;
+        }
         return score;
     }
 }
