6db931f645893ccd3566851cca695a0657634811
ejrope <ejrope@gmail.com>
2018-02-08 19:53:57 +0100

    Add testSpareFollowedByStrike - PASS

--- src/test/java/com/ejrope/katas/BowlingGame/GameTest.java
+++ src/test/java/com/ejrope/katas/BowlingGame/GameTest.java
@@ -106,6 +106,16 @@ public class GameTest {
         assertEquals(35, game.score());
     }
 
+    @Test
+    public void testSpareFollowedByStrike() {
+        rollSpare();
+        rollStrike();
+        game.roll(3);
+        game.roll(4);
+        rollMany(14,0);
+        assertEquals(44, game.score());
+    }
+
     private void rollMany(int rolls, int pins) {
         for (int i = 0; i < rolls; i++) {
             game.roll(pins);
