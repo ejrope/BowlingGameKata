b073de14dbb9362992b2536ac1d5d0ae9bbf9510
ejrope <ejrope@gmail.com>
2018-02-08 19:00:39 +0100

    Add testAllOnes - FAIL

--- src/test/java/com/ejrope/katas/BowlingGame/GameTest.java
+++ src/test/java/com/ejrope/katas/BowlingGame/GameTest.java
@@ -14,4 +14,14 @@ public class GameTest {
         }
         assertEquals(0, game.score());
     }
+
+    @Test
+    public void testAllOnes() {
+        Game game = new Game();
+
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
+    }
 }
